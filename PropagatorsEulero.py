5b5e93514d1bb08f4779525a5d16a3cf491ed1ca
marta <marta.rosa@unipd.it>
2019-09-26 11:23:34 +0200

    non va ma test new branch

--- PropagatorsEulero.py
+++ PropagatorsEulero.py
@@ -7,12 +7,12 @@ class PropagatorEulero1Order(Propagator):
         super().__init__()
 
     def set_propagator(self, dt, molecule, env):
-        self.set_attributes(dt, molecule, env)
+        self.init_propagaror_terms(dt, molecule, env)
         self.clean_propagator()
         self.add_term_to_propagator("eulero1_coeff")
         self.add_term_to_propagator("eulero_energy")
         self.add_term_to_propagator("eulero_field")
-        if self.pcm.env == "sol":
+        if self.propagator_terms.pcm.env == "sol":
             self.add_term_to_propagator("eulero_pcm")
         self.add_term_to_propagator("norm")
 
@@ -22,10 +22,10 @@ class PropagatorEulero1Order(Propagator):
 
     def propagate_n_step(self, nstep, field):
         out = list()
-        out.append(self.mol.wf.ci)
+        out.append(self.propagator_terms.mol.wf.ci)
         for i in range(nstep):
             self.propagate_one_step(field[i])
-            out.append(self.mol.wf.ci)
+            out.append(self.propagator_terms.mol.wf.ci)
         out = np.array(out)
         return out
 
@@ -36,12 +36,12 @@ class PropagatorEulero2Order(Propagator):
         super().__init__()
 
     def set_propagator(self, dt, molecule, env):
-        self.set_attributes(dt, molecule, env)
+        self.init_propagaror_terms(dt, molecule, env)
         self.clean_propagator()
         self.add_term_to_propagator("eulero2_coeff")
         self.add_term_to_propagator("eulero_energy")
         self.add_term_to_propagator("eulero_field")
-        if self.pcm.env == "sol":
+        if self.propagator_terms.pcm.env == "sol":
             self.add_term_to_propagator("eulero_pcm")
         self.add_term_to_propagator("norm")
 
@@ -52,12 +52,12 @@ class PropagatorEulero2Order(Propagator):
 
     def propagate_n_step(self, nstep, field):
         out = list()
-        out.append(self.mol.wf.ci)
+        out.append(self.propagator_terms.mol.wf.ci)
         for i in range(nstep):
             if i != 0:
                 self.propagate_one_step(field[i])
             else:
                 self.propagate_one_step(field[i], order=1)
-            out.append(self.mol.wf.ci)
+            out.append(self.propagator_terms.mol.wf.ci)
         out = np.array(out)
         return out
