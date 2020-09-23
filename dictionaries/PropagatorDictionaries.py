694269f8f834903247d93cecd8542489691df4a0
davide <davide.castaldo@studenti.unipd.it>
2020-09-23 15:46:23 +0200

    Inizio input per IBM interface

---
+++ dictionaries/PropagatorDictionaries.py
@@ -0,0 +1,16 @@
+from propagator.ClassicalPropagatorTerms import ClassicalPropagatorTerms
+from propagator.PropagatorOCRabitz import PropagatorOCfwd
+from propagator.PropagatorsEulero import PropagatorEulero2Order, PropagatorEulero1Order
+
+PropagatorDict = {
+    "eulero_2order": PropagatorEulero2Order,
+    "eulero_1order": PropagatorEulero1Order,
+    "rabitz": PropagatorOCfwd,
+    
+}
+PropagatorTermsDict = {
+    "eulero_2order": ClassicalPropagatorTerms,
+    "eulero_1order": ClassicalPropagatorTerms,
+    "rabitz": ClassicalPropagatorTerms,
+    "quantum_trotter_suzuki" : QuantumPropagatorTerms
+}
