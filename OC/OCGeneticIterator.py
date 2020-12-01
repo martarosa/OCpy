fixed sign in perturbation - fixed scipy save - small postprocessing - latin hypercube working

OC/OCGeneticIterator.py
@@ -18,6 +18,7 @@ from field.Field import Field, Func_tMatrix
 from deap import base
 from deap import creator
 from deap import tools
+from smt.sampling_methods import LHS
 #MRqiskit from qiskit.quantum_info import partial_trace
 
 import random
@@ -114,7 +115,7 @@ class OCGeneticIterator(ABCOCIterator):
         self.genetic_par.n_selected_chr = genetic_input.n_selected_chr
         self.genetic_par.amplitude_lim = genetic_input.amplitude_lim
         self.genetic_par.parallel = genetic_input.parallel
-        self.genetic_par.localized_guess = genetic_input.localized_guess
+        self.genetic_par.initial_guess = genetic_input.initial_guess
 
         self.genetic_par.mate = genetic_input.mate
         self.genetic_par.mate_probability = genetic_input.mate_probability
@@ -183,30 +184,40 @@ class OCGeneticIterator(ABCOCIterator):
     #Sono tutti uguali ma le ampiezze del mio vettore sono diverse perchè le ho fatte random prima
     # se voglio fare un guess non random ma localizzato intorno ad una soluzione approssimata metto
     # self.genetic_par.localized_guess == True
-        if self.genetic_par.localized_guess:
+        if self.genetic_par.initial_guess == 'localized':
             x = np.zeros((int(starting_field.par.control_parameters/3), 3))
             x[0,0] = 0.5
 #            x[:,1] = np.random.uniform(0, 3, int(starting_field.par.control_parameters/3))
             x[:,2] = np.random.uniform(0, 3, int(starting_field.par.control_parameters/3))
             x = np.reshape(x, starting_field.par.control_parameters).tolist()
+        elif self.genetic_par.initial_guess == 'latin_hypercube':
+            xlimits = []
+            xlimits = [[-10, 10] for k in range(self.genetic_par.n_amplitudes)]
+            xlimits = np.asarray(xlimits)
+            sampling = LHS(xlimits=xlimits, criterion = 'cm')
+            x = sampling(self.genetic_par.n_chromosomes)
         for i in range(len(self.chromosomes)):
             self.chromosomes[i].field = deepcopy(starting_field)
             self.chromosomes[i].prop_psi = deepcopy(prop_psi)
             self.chromosomes[i].J.values = [0.5]
             if (self.par.restart == 'true'):
                 self.assign_chromosome_values(self.chromosomes[i], starting_field.par.fi.reshape((1,-1)))
-            elif self.genetic_par.localized_guess:
-                self.localized_initial_guess(self.chromosomes[i], x)
+            elif self.genetic_par.initial_guess != None:
+                self.set_initial_guess(self.chromosomes[i], x)
         self.par.J = [99999]
 
-    def localized_initial_guess(self, chromosome, reshaped_initial_guess):
-        n = len(reshaped_initial_guess)
-        random_variation = int(random.randrange(0, n))
-        for j in range(n):
-            if j != random_variation:
-                chromosome[j] = reshaped_initial_guess[j] #+ random.uniform(-self.genetic_par.mutate_starting_sigma, self.genetic_par.mutate_starting_sigma)
-            else:
-                chromosome[j] = reshaped_initial_guess[j] + random.uniform(-10, 10)
+    def set_initial_guess(self, chromosome, reshaped_initial_guess):
+        if self.genetic_par.initial_guess == 'localized':
+            n = len(reshaped_initial_guess)
+            random_variation = int(random.randrange(0, n))
+            for j in range(n):
+                if j != random_variation:
+                    chromosome[j] = reshaped_initial_guess[j] #+ random.uniform(-self.genetic_par.mutate_starting_sigma, self.genetic_par.mutate_starting_sigma)
+                else:
+                    chromosome[j] = reshaped_initial_guess[j] + random.uniform(-10, 10)
+        elif self.genetic_par.initial_guess == 'latin_hypercube':
+            chromosome = reshaped_initial_guess[self.chromosomes.index(chromosome)] 
+            
                 
     def assign_chromosome_values(self, chromosome, reshaped_starting_field_fi):
         n = reshaped_starting_field_fi.size

OC/OCScipyIterator.py
@@ -23,6 +23,7 @@ import dictionaries.OCDictionaries as ocdict
 import dictionaries.PropagatorDictionaries as pdict
 import pandas as pd
 import random
+import pdb
 #MRqiskit from qiskit.quantum_info import partial_trace
 
 
@@ -66,6 +67,9 @@ class OCScipyOptimizeIterator(ABCOCIterator):
             x[:,1] = np.random.uniform(0, 3, int(self.field.par.fi.size/3))
             x[:,2] = np.random.uniform(0, 3, int(self.field.par.fi.size/3))
         x = np.reshape(x, self.field.par.fi.size).tolist()
+     #   x = np.loadtxt("/Users/castd/Desktop/Risultati_OC_GS/H2_CISD_space/Genetic_optimization/6-31G/test_new_implementation_genetic_initial_guess_allzero2_gaussian_field_bkp.dat")[10:,:]
+        x = np.reshape(x, self.field.par.fi.size).tolist()
+       # x = np.load("/Users/castd/Desktop/Risultati_OC_GS/H2_CISD_space/ScipyOptimizers/BFGS/6-31G/free_sines_perturbation/test_bfgs_single_point_H2_6-31g_free_sines_15par0.74_result.npy", allow_pickle=True)[0].x.tolist()
         return x
     
     ### prendo spunto da restart_genetic (ma meglio), posso dargli
@@ -123,6 +127,7 @@ class OCScipyOptimizeIterator(ABCOCIterator):
                 self.field.par.fi = np.asarray(coefficients).reshape((-1, 1))
             else:
                 self.field.par.fi = np.asarray(coefficients).reshape((-1, 3))
+               # print(self.field.par.fi)
             self.field.chose_field(self.field.par.field_type, discrete_t_par = self.discrete_t_par)
         self.prop_psi.mol.wf.set_wf(self.initial_c0, 1)
         if self.par.propagator == 'quantum_trotter_suzuki':
@@ -142,6 +147,15 @@ class OCScipyOptimizeIterator(ABCOCIterator):
             '''
         else:
             self.prop_psi.propagate_n_step(self.discrete_t_par, self.field.field)
+#            trajectory = self.prop_psi.propagate_n_step(self.discrete_t_par, self.field.field)
+#            to_save = []
+#            to_save.append(trajectory)
+#            to_save.append(self.field.field.f_xyz)
+#            to_save.append(self.prop_psi.mol.par.hamiltonian)
+#            to_save.append(self.prop_psi.mol.par.control_operator)
+#            np.save("postprocessing_material_bfgs_45par.npy", to_save)
+#            pdb.set_trace()
+            # continua qua
             if self.par.control_problem == "optical_excitation":
                 self.obj_fun.compute_objective_function(self, self.prop_psi.mol.wf.ci, self.par.target_state)
                 J = np.real(self.obj_fun.J)
@@ -191,10 +205,12 @@ class OCScipyOptimizeIterator(ABCOCIterator):
 ### methods to save the output #####
         
     def get_field_ampl(self):
-         if self.current_iteration - self.par.n_iterations + 1 <= len(self.amplitudes_to_save):
-             return np.reshape(self.amplitudes_to_save[self.current_iteration - self.par.n_iterations + 1], ((-1,3)))
-         else:
-             return np.reshape(self.amplitudes_to_save[-1], ((-1,3)))
+        print(len(self.amplitudes_to_save))
+        print(self.current_iteration - self.par.n_iterations + 1)
+        if self.current_iteration - self.par.n_iterations + 1 < len(self.amplitudes_to_save):
+            return np.reshape(self.amplitudes_to_save[self.current_iteration - self.par.n_iterations + 1], ((-1,3)))
+        else:
+            return np.reshape(self.amplitudes_to_save[-1], ((-1,3)))
     
     
     def field_J_integral(self, amplitudes):
@@ -209,14 +225,18 @@ class OCScipyOptimizeIterator(ABCOCIterator):
 
     
     def get_log_file_out(self): 
-        if self.current_iteration - self.par.n_iterations + 1 < len(self.pop_target):
-            pop_target = self.pop_target[self.current_iteration - self.par.n_iterations + 1]
-            integral_field = self.field_J_integral(self.amplitudes_to_save[self.current_iteration - self.par.n_iterations + 1]) 
-        else:
-            pop_target = self.pop_target[-1]
-            integral_field = self.field_J_integral(self.amplitudes_to_save[-1])
-        self.check_convergence()
-        log_array = np.array([self.par.convergence_t, self.par.J[self.current_iteration - self.par.n_iterations + 1], pop_target, integral_field])
+        if self.par.control_problem == 'optical_excitation':
+            if self.current_iteration - self.par.n_iterations + 1 < len(self.pop_target):
+                pop_target = self.pop_target[self.current_iteration - self.par.n_iterations + 1]
+                integral_field = self.field_J_integral(self.amplitudes_to_save[self.current_iteration - self.par.n_iterations + 1]) 
+            else:
+                pop_target = self.pop_target[-1]
+                integral_field = self.field_J_integral(self.amplitudes_to_save[-1])
+            self.check_convergence()
+            log_array = np.array([self.par.convergence_t, self.par.J[self.current_iteration - self.par.n_iterations + 1], pop_target, integral_field])
+        elif self.par.control_problem == 'ground_state':
+            self.check_convergence()
+            log_array = np.array([self.par.convergence_t, self.par.J[self.current_iteration - self.par.n_iterations + 1]])
         self.current_iteration += 1
         return log_array
 

field/Field.py
@@ -133,8 +133,8 @@ class Field():
         self.field.f_xyz = np.zeros([discrete_t_par.nstep, 1])
         for i in range(discrete_t_par.nstep):
             for k in range(int(self.par.control_parameters/3)):
-               # self.field.f_xyz += self.par.fi[k, 0] * np.cos(self.par.fi[k, 1] * i * discrete_t_par.dt) + self.par.fi[k, 2] * np.cos(self.par.fi[k, 1] * i * discrete_t_par.dt)
-               self.field.f_xyz += self.par.fi[k,0] * np.cos((self.par.fi[k,1] * i * discrete_t_par.dt + self.par.fi[k,2]))
+                #self.field.f_xyz[i] += self.par.fi[k, 0] * np.cos(self.par.fi[k, 1] * i * discrete_t_par.dt) + self.par.fi[k, 2] * np.cos(self.par.fi[k, 1] * i * discrete_t_par.dt)
+               self.field.f_xyz[i] += self.par.fi[k,0] * np.cos((self.par.fi[k,1] * i * discrete_t_par.dt + self.par.fi[k,2]))
 
 
     def genetic_pulse(self, discrete_t_par):

propagator/ClassicalPropagatorTerms.py
@@ -45,7 +45,7 @@ class ClassicalPropagatorTerms(ABCPropagatorTerms):
         
     def eulero_external_hamiltonian_term(self, mol, order, dt, perturbation, *args):
         mol.propagate_hamiltonian(perturbation)
-        mol.wf.ci += -order * 1j * dt * (np.dot(mol.par.hamiltonian_t, mol.wf.ci_prev[0]))
+        mol.wf.ci += -order * 1j * dt * (np.dot(mol.wf.ci_prev[0], mol.par.hamiltonian_t))
         
 #    def eulero_system_hamiltonian_term(self, mol, order, dt, *args):
 #        mol.wf.ci += -order * 1j * dt * (np.dot(mol.wf.ci_prev[0], mol.par.hamiltonian))